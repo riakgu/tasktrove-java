40cfe86af6d6d04ae8ba5a21f0ec74df4e27291e
riakgu <rizkyakbargusnaen@gmail.com>
2023-12-20 09:17:24 +0700

    task controller

--- src/tasktrove/controller/TaskController.java
+++ src/tasktrove/controller/TaskController.java
@@ -5,10 +5,57 @@
  */
 package tasktrove.controller;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+import tasktrove.model.User;
+import tasktrove.util.Database;
+
 /**
  *
  * @author riakgu
  */
 public class TaskController {
+    private User user;
     
+    public TaskController(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+    
+    public ResultSet getTasks() {
+        ResultSet rs = null;
+        try {
+            Connection connection = Database.getConnection();
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM tasks WHERE user_id = ?");
+            ps.setInt(1, user.getUser_id());
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+    
+    public void loadTasksIntoTable(DefaultTableModel model) {
+        try {
+            ResultSet rs = getTasks();
+            while (rs != null && rs.next()) {
+                String taskName = rs.getString("task_name");
+                String description = rs.getString("description");
+                Date started = rs.getDate("started");
+                Date deadline = rs.getDate("deadline");
+                String status = rs.getString("status");
+
+                model.addRow(new Object[]{taskName, description, started, deadline, status});
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
